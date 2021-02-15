387ae47e52e61e86fcca8f0b04a056679a673417
Leandro <leodolz14@gmail.com>
2021-02-15 21:58:58 +0000

    Add code to finalize implementation for project

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -45,17 +45,14 @@ class App extends React.Component {
     }
     ret = axios.get(url);
     ret.then((response)=>{
-
-      //Include code here to check the sentiment and fomrat the data accordingly
-
-      this.setState({sentimentOutput:response.data});
-      let output = response.data;
-      if(response.data === "positive") {
-        output = <div style={{color:"green",fontSize:20}}>{response.data}</div>
-      } else if (response.data === "negative"){
-        output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
+      this.setState({sentimentOutput:response.data.label});
+      let output = response.data.label;
+      if(response.data.label === "positive") {
+        output = <div style={{color:"green",fontSize:20}}>{response.data.label}</div>
+      } else if (response.data.label === "negative"){
+        output = <div style={{color:"red",fontSize:20}}>{response.data.label}</div>
       } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+        output = <div style={{color:"yellow",fontSize:20}}>{response.data.label}</div>
       }
       this.setState({sentimentOutput:output});
     });
--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -5,18 +5,27 @@ class EmotionTable extends React.Component {
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+                this.getMappedEmotions()
             }
             </tbody>
           </table>
           </div>
           );
         }
+
+    getMappedEmotions()
+    {
+        let emotionsArray = Object.entries(this.props.emotions);
+        return emotionsArray.map((emotionItem) => {
+            return <tr>
+                <td>{emotionItem[0]}</td>
+                <td>{emotionItem[1]}</td>
+            </tr>;
+        });
+    }
     
 }
 export default EmotionTable;
